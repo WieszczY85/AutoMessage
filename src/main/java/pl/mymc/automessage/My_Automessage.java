90718c79686a630ff93d7ae86f6faf31bea63c8b
WieszczY <wieszczy85@gmail.com>
2024-05-24 20:44:32 +0200

    Update

--- src/main/java/pl/mymc/automessage/My_Automessage.java
+++ src/main/java/pl/mymc/automessage/My_Automessage.java
@@ -1,12 +1,16 @@
 package pl.mymc.automessage;
 
 import me.clip.placeholderapi.PlaceholderAPI;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.configuration.file.FileConfiguration;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.Component;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
@@ -18,6 +22,7 @@ public class My_Automessage extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        getLogger().info("My-AutoMessage włączony.");
         saveDefaultConfig();
         config = getConfig();
         messages = config.getStringList("auto-message.messages");
@@ -29,6 +34,22 @@ public class My_Automessage extends JavaPlugin {
         if (autoMessageTask != null) {
             autoMessageTask.cancel();
         }
+    }
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, Command command, @NotNull String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("reload")) {
+            if (sender.hasPermission("myautomessage.reload")) {
+                reloadPluginConfig();
+                sender.sendMessage(NamedTextColor.GREEN + "Konfiguracja została przeładowana.");
+            } else {
+                sender.sendMessage(NamedTextColor.RED + "Nie masz uprawnień do wykonania tej komendy.");
+            }
+            return true;
+        }
+        return false;
+    }
+    public void reloadPluginConfig() {
+        reloadConfig();
     }
 
     private void startAutoMessageTask() {
@@ -45,8 +66,10 @@ public class My_Automessage extends JavaPlugin {
 
                         fullMessage = PlaceholderAPI.setPlaceholders(null, fullMessage);
                     } else {
-                    getLogger().warning("Zapomniałeś dodać PlaceholderAPI na serwer, kretynie?.");
+                        getLogger().warning("Nie znalazłem PlaceholderAPI na serwerze.");
                     }
+                } else {
+                    getLogger().warning("Nie znalazłem  włączonego PlaceholderAPI na serwerze.");
                 }
                 MiniMessage miniMessage = MiniMessage.miniMessage();
                 Component messageComponent = miniMessage.deserialize(fullMessage);
