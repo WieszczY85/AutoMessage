a19b24a56138229f0cce83d7963327420b94e4e2
WieszczY <wieszczy85@gmail.com>
2024-05-24 21:47:20 +0200

    Update

--- src/main/java/pl/mymc/automessage/ReloadCommand.java
+++ src/main/java/pl/mymc/automessage/ReloadCommand.java
@@ -3,16 +3,17 @@ package pl.mymc.automessage;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
 
 public class ReloadCommand implements CommandExecutor {
-    private final My_Automessage plugin;
+    private My_Automessage plugin;
 
     public ReloadCommand(My_Automessage plugin) {
         this.plugin = plugin;
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    public boolean onCommand(CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         if (sender.hasPermission("myautomessage.reload")) {
             plugin.reloadConfig();
             sender.sendMessage("Konfiguracja została ponownie załadowana!");
