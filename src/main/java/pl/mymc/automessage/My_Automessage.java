8f428903dec9fbaccd44dfc74c20511c9c89a28d
WieszczY <wieszczy85@gmail.com>
2024-05-24 20:24:44 +0200

    Update

--- src/main/java/pl/mymc/automessage/My_Automessage.java
+++ src/main/java/pl/mymc/automessage/My_Automessage.java
@@ -2,7 +2,6 @@ package pl.mymc.automessage;
 
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.configuration.file.FileConfiguration;
@@ -41,24 +40,21 @@ public class My_Automessage extends JavaPlugin {
                 String prefix = config.getString("prefix");
                 String message = messages.get(messageIndex);
                 String fullMessage = prefix + message;
-                MiniMessage miniMessage = MiniMessage.miniMessage();
-                Component messageComponent = miniMessage.deserialize(fullMessage);
+                if(Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")){
+                    if(Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null){
 
-                for (Player player : getServer().getOnlinePlayers()) {
-                    if(Bukkit.getPluginManager().isPluginEnabled("PlaceholderAPI")){
-                        String personalizedMessage = PlaceholderAPI.setPlaceholders(player, fullMessage);
-                        Component personalizedComponent = miniMessage.deserialize(personalizedMessage);
-                        player.sendMessage(personalizedComponent);
+                        fullMessage = PlaceholderAPI.setPlaceholders(null, fullMessage);
                     } else {
-                        player.sendMessage(messageComponent);
+                    getLogger().warning("Zapomniałeś dodać PlaceholderAPI na serwer, kretynie?.");
                     }
                 }
-
+                MiniMessage miniMessage = MiniMessage.miniMessage();
+                Component messageComponent = miniMessage.deserialize(fullMessage);
+                getServer().broadcast(messageComponent);
                 messageIndex = (messageIndex + 1) % messages.size();
             }
         };
 
         autoMessageTask.runTaskTimer(this, 0L, interval);
     }
-
 }
